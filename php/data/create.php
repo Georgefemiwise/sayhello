8df9e2031eebb08ae794da3cfa28bcc45153469e
georgefemiwise <georgewise1010@gmail.com>
2023-02-15 16:33:11 +0000

    cleaning

--- php/data/create.php
+++ php/data/create.php
@@ -8,8 +8,6 @@
      //    $address = $_REQUEST['address'];
      //    $email = $_REQUEST['email'];
          
-        // Performing insert query execution
-        // here our table name is college
 
 $year_enrolled = 21;
 $school = 'ttu';
@@ -25,30 +23,14 @@ for ($index = 1; $index <= 200; $index++ ){
     
     if(!$result){
         die('Invalid Query: ');
+    }else{
+        echo "<script>alert('$school email for $program is created..')</script>";
+        header("location: ../dashboard.php");
+        
     }
 }
 
 
-
-// "INSERT INTO home (name)  
-//     VALUES ('$index')";
-//         array_push($email_address,"bcict21".(sprintf('%03d', $index))."@ttu.edu.gh");
-
-	echo "<div id='demo'></div>";
-
-
-
-
-
-// if(mysqli_query($conn, $sql)){
-// // header("Location:");
-// // header("location: ../dashboard.php");
-// }
-// else{
-// echo "ERROR: Hush! Sorry $sql. "
-// . mysqli_error($conn);
-// }
-// Close connection
 mysqli_close($conn);
 
 
@@ -66,11 +48,3 @@ mysqli_close($conn);
 // $addres =[];
 // for ($i=1; $i <= 100; $i++){ // array_push($addres,$i); // } // foreach($addres as $fruits){ // echo sprintf('%03d',
 	// $fruits) . "<br>" ; // } ?>
-<script type="text/JavaScript">
-
-
-	// 	const text='';
-// for (let i = 0; i < 10; i++) {
-//   console.log(`bcict21${i}@ttu.edu.gh`);
-// }
-</script>
