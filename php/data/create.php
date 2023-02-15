7769769a0efacd8540028ab4ffc670bc51dc32b6
georgefemiwise <georgewise1010@gmail.com>
2023-02-15 16:27:20 +0000

    completed: email generator

--- php/data/create.php
+++ php/data/create.php
@@ -2,42 +2,75 @@
   require 'connection/database_connector.php';
 
  // Taking all 5 values from the form data(input)
-        $first_name =  $_REQUEST['first_name'];
-        $last_name = $_REQUEST['last_name'];
-        $gender =  $_REQUEST['gender'];
-        $address = $_REQUEST['address'];
-        $email = $_REQUEST['email'];
+     //    $first_name =  $_REQUEST['first_name'];
+     //    $last_name = $_REQUEST['last_name'];
+     //    $gender =  $_REQUEST['gender'];
+     //    $address = $_REQUEST['address'];
+     //    $email = $_REQUEST['email'];
          
         // Performing insert query execution
         // here our table name is college
-        $sql = "INSERT INTO college  VALUES ('$first_name',
-            '$last_name','$gender','$address','$email')";
-         
-        if(mysqli_query($conn, $sql)){
-            echo "<h3>data stored in a database successfully."
-                . " Please browse your localhost php my admin"
-                . " to view the updated data</h3>";
- 
-            echo nl2br("\n$first_name\n $last_name\n "
-                . "$gender\n $address\n $email");
-        } else{
-            echo "ERROR: Hush! Sorry $sql. "
-                . mysqli_error($conn);
-        }
-         
-        // Close connection
-        mysqli_close($conn);
+
+$year_enrolled = 21;
+$school = 'ttu';
+$program = 'computer science';
 
 
-
-  $addres =[];
-for ($i=1; $i <= 100; $i++){
-	
-	
-	array_push($addres,$i);
-  
+for ($index = 1; $index <= 200; $index++ ){
+    
+    $re_index = sprintf('%03d',$index);
+    $sql = "INSERT INTO student (email_address, school, program, year_enrolled) 
+    VALUES ('bcict21$re_index@ttu.edu.gh','$school','$program','$year_enrolled')";
+    $result = mysqli_query($conn, $sql);
+    
+    if(!$result){
+        die('Invalid Query: ');
+    }
 }
-foreach($addres as $fruits){
-		    echo sprintf('%03d', $fruits) . "<br>";
-		}
-?>
+
+
+
+// "INSERT INTO home (name)  
+//     VALUES ('$index')";
+//         array_push($email_address,"bcict21".(sprintf('%03d', $index))."@ttu.edu.gh");
+
+	echo "<div id='demo'></div>";
+
+
+
+
+
+// if(mysqli_query($conn, $sql)){
+// // header("Location:");
+// // header("location: ../dashboard.php");
+// }
+// else{
+// echo "ERROR: Hush! Sorry $sql. "
+// . mysqli_error($conn);
+// }
+// Close connection
+mysqli_close($conn);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $addres =[];
+// for ($i=1; $i <= 100; $i++){ // array_push($addres,$i); // } // foreach($addres as $fruits){ // echo sprintf('%03d',
+	// $fruits) . "<br>" ; // } ?>
+<script type="text/JavaScript">
+
+
+	// 	const text='';
+// for (let i = 0; i < 10; i++) {
+//   console.log(`bcict21${i}@ttu.edu.gh`);
+// }
+</script>
