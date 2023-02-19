b038949d9d2868c53ddf34d9ca0c02f3e2e28ba1
georgefemiwise <georgewise1010@gmail.com>
2023-02-19 00:21:27 +0000

    database restructured

--- php/dashboard.php
+++ php/dashboard.php
@@ -1,15 +1,25 @@
-<?php
-  require './data/connection/database_connector.php';
-echo '<h1> dashborad </h1>';
+<!DOCTYPE html>
+<html lang="en">
 
-$total_row_sql = "SELECT * from student";
+	<head>
+		<?php require './data/connection/database_connector.php';?>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Document</title>
+	</head>
 
-if ($result = mysqli_query($conn, $total_row_sql)) {
+	<body>
 
-    // Return the number of rows in result set
-    $rowcount = mysqli_num_rows( $result );
-    
-    // Display result
-    printf("Total rows in this table :  %d\n", $rowcount);
- }
-?>
+		<?php 
+		$total_row_sql = "SELECT * from student";
+		if ($result = mysqli_query($conn, $total_row_sql)) {
+
+			// Return the number of rows in result set
+			$rowcount = mysqli_num_rows( $result );
+	
+			// Display result
+			printf("Total rows in this table :  %d\n", $rowcount);}?>
+	</body>
+
+</html>
--- php/data/create.php
+++ php/data/create.php
@@ -1,38 +1,47 @@
 <?php
   require 'connection/database_connector.php';
 
- // Taking all 5 values from the form data(input)
-     //    $first_name =  $_REQUEST['first_name'];
-     //    $last_name = $_REQUEST['last_name'];
-     //    $gender =  $_REQUEST['gender'];
-     //    $address = $_REQUEST['address'];
-     //    $email = $_REQUEST['email'];
-         
-
-$year_enrolled = 21;
-$school = 'ttu';
-$program = 'computer science';
+//  Taking all 5 values from the form data(input)
 
 
-for ($index = 1; $index <= 200; $index++ ){
+        $school_Name =  $_POST['schName'];
+        $school_location = $_POST['school_location'];
+        $student_name =  $_POST['student_name'];
+        $program = $_POST['program'];
+        $year_enrolled = $_POST['year'];
+
+        
+         echo   " $school_Name $school_location $student_name $program $year";
+
+// $year_enrolled = 21;
+// $school = 'ttu';
+// $program = 'bcict';
+
+
+   $school_sql = "INSERT INTO school (name, location)
+           VALUES ('$school_Name','$school_location')";
+
+
+           
+// for ($index = 1; $index <= 200; $index++ ){
     
-    //leading zeros
-    $re_index = sprintf('%03d',$index);
+//     // leading zeros
+//    $re_index = sprintf('%03d',$index);
     
-    //sql insert into database
-    $sql = "INSERT INTO student (email_address, school, program, year_enrolled)
-            VALUES ('bcict21$re_index@ttu.edu.gh','$school','$program','$year_enrolled')";
+    // sql insert into database
+   $student_sql = "INSERT INTO student (name, email_address, school_id, program, year)
+           VALUES ('$program$year@ttu.edu.gh','$school_Name','$program','$year_enrolled')";
     
     $result = mysqli_query($conn, $sql);
     
-    if(!$result){
-        die('Invalid Query: ');
-    }else{
-        echo "<script>alert('$school email for $program is created..')</script>";
-        header("location: ../dashboard.php");
+    // if(!$result){
+    //     die('Invalid Query: ');
+    // }else{
+    //     echo "<script>alert('$school email for $program is created..');</script>";
+    //     header("location: ../dashboard.php");
         
-    }
-}
+//     }
+// }
 
 
 mysqli_close($conn);
