70699bcbe55346cd234879104354c9516d7ef376
georgefemiwise <georgewise1010@gmail.com>
2023-02-21 01:40:40 +0000

    in progress

--- php/data/create.php
+++ php/data/create.php
@@ -1,9 +1,5 @@
 <?php
-  require 'connection/database_connector.php';
-
 //  Taking all 5 values from the form data(input)
-
-
         $school_Name =  $_POST['schName'];
         $school_location = $_POST['school_location'];
         $student_name =  $_POST['student_name'];
@@ -11,11 +7,24 @@
         $year_enrolled = $_POST['year'];
 
         
-         echo   " $school_Name $school_location $student_name $program $year";
+        function sqlSubmit($QueryServer){
+            require 'connection/database_connector.php';
+             
+            $done=mysqli_query($conn, $QueryServer);
+            if(!$done){
+                    die('Invalid Query');}
+            else{
+                echo "<script>alert('Saved');</script>";
+       }
+            mysqli_close($conn);
+        }
 
-// $year_enrolled = 21;
-// $school = 'ttu';
-// $program = 'bcict';
+
+
+
+  
+        echo   " $school_Name $school_location $student_name $program $year_enrolled";
+
 
 
    $school_sql = "INSERT INTO school (name, location)
@@ -28,21 +37,117 @@
 //     // leading zeros
 //    $re_index = sprintf('%03d',$index);
     
-    // sql insert into database
+//     // sql insert into database
    $student_sql = "INSERT INTO student (name, email_address, school_id, program, year)
-           VALUES ('$program$year@ttu.edu.gh','$school_Name','$program','$year_enrolled')";
+ 
+            VALUES ('$student_name','$program$year_enrolled@ttu.edu.gh','$school_Name','$program','$year_enrolled')";
     
-    $result = mysqli_query($conn, $sql);
+
+    sqlSubmit($school_sql );
+    sqlSubmit($student_sql);
+    echo "
+		<div class='body'>
+			<div class='loading'>
+				<h4>Loading...</h4>
+			</div>
+		</div>
+		
+
+		<style>
+		
+			.body {
+				position: absolute;
+				width: 100vw;
+				height: 95vh;
+				display: grid;
+				place-content: center;
+				background-color: rgba(24, 20, 255, 0.863);
+
+			}
+
+			.loading {
+				position: relative;
+				display: grid;
+				place-content: center;
+				height: 30px;
+				color: rgb(244, 243, 248);
+				font-size: larger;
+			}
+
+			.loading::after {
+				position: absolute;
+				content: '';
+				width: 15px;
+				height: 4px;
+				background-color: rgb(246, 0, 0);
+				bottom: 0;
+				animation: loader linear infinite 2s;
+			}
+
+			@keyframes loader {
+				0% {
+					background-color: #ff0000;
+				}
+
+				30% {
+					background-color: #00f80c;
+				}
+
+				60% {
+					transform: translateX(50px);
+					background-color: rgb(255, 0, 217);
+				}
+
+				100% {
+					background-color: #f3e40d;
+				}
+
+			}
+
+		</style>
+	";
+    // header("location: ../dashboard.php");
     
-    // if(!$result){
-    //     die('Invalid Query: ');
-    // }else{
-    //     echo "<script>alert('$school email for $program is created..');</script>";
-    //     header("location: ../dashboard.php");
-        
-//     }
-// }
 
 
-mysqli_close($conn);
- ?>
+
+$severname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "proj";
+
+
+//Create connection
+
+
+$conn = new mysqli($severname, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+
+
+session_start();
+include('input.php');
+
+$user_id=$_POST['user_id'];
+$user_name=$_POST['user_name'];
+$proj_id=$_POST['proj_id'];
+
+
+$qry="SELECT * FROM proj WHERE proj_id=$proj_id";
+$result = mysqli_query($qry);
+$num_rows = mysqli_num_rows($result);
+
+if($num_rows > 0){
+    mysqli_query("INSERT INTO user(user_id, user_name, proj_id) 
+    VALUES('$user_id', '$user_name', $proj_id)");
+}else{
+    echo "Team is not valid!!!";
+}
+mysqli_close($con);
+header("location: input.php?remarks=success"); 
+
+mysqli_error($con); 
+?>
