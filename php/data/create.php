7c56fe31d1885aa3ff5717cc577a46d81ceb7b10
georgefemiwise <georgewise1010@gmail.com>
2023-02-21 17:25:32 +0000

    loader

--- php/data/create.php
+++ php/data/create.php
@@ -1,4 +1,19 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Document</title>
+	</head>
+
+	<body>
+
+	</body>
+
+</html><?php
+
 //  Taking all 5 values from the form data(input)
         $school_Name =  $_POST['schName'];
         $school_location = $_POST['school_location'];
